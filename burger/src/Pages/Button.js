34df56118a816f065b346137218a2cdeca51e85f
SOVEREIGN <isimemeni@gmail.com>
2020-11-19 22:47:06 -0500

    working with input change function

--- burger/src/Pages/Button.js
+++ burger/src/Pages/Button.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-class Button extends React.Component{
+class Button extends Component{
 
 state = {
     talley: 0
 }
+
+handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+        [name]: value
+    });
+    console.log(value);
+};
+
 
 counterUp = () => {
     this.setState({
@@ -24,7 +33,8 @@ render () {
     <div>
         <button onClick={this.counterUp}>Increases by one</button> 
         <button onClick={this.counterDown}>Decreases by one</button>
-        <p>{this.state.talley}</p>    
+        <p>{this.state.talley}</p> 
+        <input onChange={this.handleInputChange}></input>   
     </div>
     )
 }
